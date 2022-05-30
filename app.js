eb47fd30444a2695bee56af7d5a8f276e9ff8d40
ilysim <illymarie1@live.com>
2022-05-30 11:03:39 -0500

    figure out step 3

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+  prevGuesses: [],
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+      console.log(this.secretNum)
   }
 }
+// method = function that is attached to an object
+game.getGuess = function(guess){
+  while (guess >= [smallestNum] || guess <= [biggestNum]) 
+  prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`)
+}
+console.log(game)
+
+
+// game.play()
